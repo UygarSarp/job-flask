dffcaa0bef7a39e57067b78b9d43aa031613ea35
uygar <uygargurnal2@gmail.com>
2024-09-29 01:19:08 +0300

    initial d

--- main.py
+++ main.py
@@ -1,11 +1,60 @@
-from flask import Flask, render_template
+import os
+from datetime import datetime
+from flask import Flask, render_template, request, flash
+from flask_sqlalchemy import SQLAlchemy
+from flask_mail import Mail, Message
 
 app = Flask(__name__)
+password = os.getenv("PASS3")
+app.config["SECRET_KEY"] = "piton123"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
+app.config["MAIL_SERVER"] = "smtp.gmail.com"
+app.config["MAIL_PORT"] = 465
+app.config["MAIL_USE_SSL"] = True
+app.config["MAIL_USERNAME"] = "infocukamuran@gmail.com"
+app.config["MAIL_PASSWORD"] = password
+
+db = SQLAlchemy(app)
+
+mail = Mail(app)
 
 
-@app.route("/")
+class Form(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(80))
+    last_name = db.Column(db.String(80))
+    email = db.Column(db.String(80))
+    date = db.Column(db.Date)
+    occupation = db.Column(db.String(80))
+
+
+@app.route("/", methods=["GET", "POST"])
 def index():
+    if request.method == "POST":
+        first_name = request.form["first_name"]
+        last_name = request.form["last_name"]
+        email = request.form["email"]
+        date = request.form["date"]
+        date_obj = datetime.strptime(date, "%Y-%m-%d")
+        occupation = request.form["occupation"]
+
+        form = Form(first_name=first_name, last_name=last_name, email=email, date=date_obj, occupation=occupation)
+        db.session.add(form)
+        db.session.commit()
+        message_body = f"""Thank you for your submission {first_name}.
+{date}"""
+        message = Message(subject="New form submission",
+                          sender=app.config["MAIL_USERNAME"],
+                          recipients=[email],
+                          body=message_body)
+
+        mail.send(message)
+        flash(f"{first_name}, your form was submitted successfully!")
+
     return render_template("index.html")
 
 
-app.run(debug=True)
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
+        app.run(debug=True)
